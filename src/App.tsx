8385f5e320a474e63b4df693e8165483f556515a
thesalmankhxn <thesalmankhxn@gmail.com>
2024-06-02 04:36:54 +0530

    wip: setup aceternity ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
 import "./App.css";
+import { Globe } from "./Components/ui/Globe/globe";
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <main>
+      <h1 className="text-[20px] font-bold montserrat">SK77 NEWS+</h1>
+
+      <h2>The stories you need to hear. Everywhere you want to listen.</h2>
+
+      <Globe
+        globeConfig={{
+          pointSize: undefined,
+          globeColor: undefined,
+          showAtmosphere: undefined,
+          atmosphereColor: undefined,
+          atmosphereAltitude: undefined,
+          emissive: undefined,
+          emissiveIntensity: undefined,
+          shininess: undefined,
+          polygonColor: undefined,
+          ambientLight: undefined,
+          directionalLeftLight: undefined,
+          directionalTopLight: undefined,
+          pointLight: undefined,
+          arcTime: undefined,
+          arcLength: undefined,
+          rings: undefined,
+          maxRings: undefined,
+          initialPosition: undefined,
+          autoRotate: undefined,
+          autoRotateSpeed: undefined,
+        }}
+        data={[]}
+      />
+    </main>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,123 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
+import colors from "tailwindcss/colors";
+import flattenColorPalette from "tailwindcss/lib/util/flattenColorPalette";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "4xl": { min: "1481px" },
+        xxxl: { max: "1480px" },
+        xxl: { max: "1280px" },
+        xl: { max: "1200px" },
+        lg: { max: "1080px" },
+        md: { max: "980px" },
+        xmd: { max: "920px" },
+        xxmd: { max: "780px" },
+        sm: { max: "640px" },
+        xsm: { max: "540px" },
+        xxsm: { max: "480px" },
+        "min-sm": { min: "640px" },
+        "min-xxmd": { min: "780px" },
+        "min-md": { min: "980px" },
+        "min-lg": { min: "1080px" },
+        "min-xl": { min: "1200px" },
+        "min-xxl": { min: "1280px" },
+        "min-xxxl": { min: "1480px" },
+        tall: { raw: "(min-height: 730px)" },
+        "tall-md": { raw: "(min-height: 800px)" },
+      },
+      colors: {
+        black: {
+          1: "#141517",
+          2: "#20201f",
+        },
+        gray: {
+          1: "#d3d7e2",
+          2: "#f5f7fc",
+          3: "#eff0f6",
+        },
+        blue: {
+          1: "#5167f6",
+          cta: "#4054fa",
+          hover: "#2c41f0",
+          2: "#4a4fcb",
+          3: "#5d9cf4",
+          4: "#76818b",
+        },
+        pink: {
+          1: "#d8578a",
+        },
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      animation: {
+        first: "moveVertical 30s ease infinite",
+        second: "moveInCircle 20s reverse infinite",
+        third: "moveInCircle 40s linear infinite",
+        fourth: "moveHorizontal 40s ease infinite",
+        fifth: "moveInCircle 20s ease infinite",
+        scroll:
+          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+      },
+      keyframes: {
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
+        moveHorizontal: {
+          "0%": {
+            transform: "translateX(-50%) translateY(-10%)",
+          },
+          "50%": {
+            transform: "translateX(50%) translateY(10%)",
+          },
+          "100%": {
+            transform: "translateX(-50%) translateY(-10%)",
+          },
+        },
+        moveInCircle: {
+          "0%": {
+            transform: "rotate(0deg)",
+          },
+          "50%": {
+            transform: "rotate(180deg)",
+          },
+          "100%": {
+            transform: "rotate(360deg)",
+          },
+        },
+        moveVertical: {
+          "0%": {
+            transform: "translateY(-50%)",
+          },
+          "50%": {
+            transform: "translateY(50%)",
+          },
+          "100%": {
+            transform: "translateY(-50%)",
+          },
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 };
+
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
