0c40a29f6960ac34c72fb036ea1dbc691f6af85c
thesalmankhxn <thesalmankhxn@gmail.com>
2024-06-04 20:23:43 +0530

    fix: remove

--- src/App.tsx
+++ src/App.tsx
@@ -30,29 +30,4 @@ function App() {
   );
 }
 
-{
-  /* <motion.div
-          initial={{
-            opacity: 0,
-            y: 20,
-          }}
-          animate={{
-            opacity: 1,
-            y: 0,
-          }}
-          transition={{
-            duration: 1,
-          }}
-          className="div"
-        >
-          <h2 className="text-center text-xl md:text-4xl font-bold text-black dark:text-white">
-            We sell soap worldwide
-          </h2>
-          <p className="text-center text-base md:text-lg font-normal text-neutral-700 dark:text-neutral-200 max-w-md mt-2 mx-auto">
-            This globe is interactive and customizable. Have fun with it, and
-            don&apos;t forget to share it. :)
-          </p>
-        </motion.div> */
-}
-
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,9 @@ export default defineConfig({
   },
   server: {
     host: true,
+    port: 8000,
+    watch: {
+      usePolling: true,
+    },
   },
 });
